f1d6ad84cbb296e5bca456c8c39fb3e56cca1bae
pramnegi <negi.1984@gmail.com>
2018-04-03 23:54:39 -0700

    Update Stringpermute.cpp

--- Stringpermute.cpp
+++ Stringpermute.cpp
@@ -7,50 +7,33 @@ input: 1. no of test cases
 	2. string
 */
 
-void swp(char *a, char *b)
-{
-	char temp;
-	temp = *a;
+void swp(char *a, char *b) {
+	char temp = *a;
 	*a = *b;
 	*b= temp;
 }
 
 
 int flag=0;
-void print(string s, int l, int r)
-{
-int i;
-if (l == r)
-	cout<<s<<endl;
-else
-{
-	for (i = l; i <= r; i++)
-	{
-	    /*for(int j=l;j<i;j++)
-        {
-            if(s[i]==s[j])
-                flag=1;
-            flag=0;
-        }
-	    if(flag==0)*/
-	    //{
+void print(string s, int l, int r) {
+	int i;
+	if (l == r) {
+		cout<<s<<endl;
+		return;
+	}
+	for (i = l; i <= r; i++) {
 		swp(&(s[l]), &(s[i]));
 		print(s, l+1, r);
 		swp(&(s[l]), &(s[i]));
-	    //}
-
 	}
 }
-}
 
 
-int main()
-{
+int main() {
 	int t;
 	string s;
 	cin>>t;
-	while(t--)
-    {
+	while(t--) {
     cin.clear();
     cin>>s;
 	int n = s.length();
