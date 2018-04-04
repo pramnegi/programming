b0b5a7b48e7153cf0d81a6db8333e8ab6b62af06
pramnegi <negi.1984@gmail.com>
2018-04-03 23:56:51 -0700

    Update distinctspermutation.cpp

--- distinctspermutation.cpp
+++ distinctspermutation.cpp
@@ -6,45 +6,35 @@ approach: recursion
 input: 1. no of test cases
 	2. string
 */
-void swp(char *a, char *b)
-{
-	char temp;
-	temp = *a;
+void swp(char *a, char *b) {
+	char temp = *a;
 	*a = *b;
 	*b= temp;
 }
 
 
-int flag=0;
-void print(string s, int l, int r)
-{
-int i;
-if (l == r)
-	cout<<s<<endl;
-else
-{
-	for (i = l; i <= r; i++)
-	{
-	    for(int j=l;j<i;j++)
-        {
+int flag=0; // this flag is global, avoid global or static variables
+void print(string s, int l, int r) {
+	if (l == r) {
+		cout<<s<<endl;
+		return;
+	}
+	for (int i = l; i <= r; i++) {
+	    for(int j=l;j<i;j++) {
             if(s[i]==s[j])
                 flag=1;
             flag=0;
         }
-	    if(flag==0)
-	    {
-		swp(&(s[l]), &(s[i]));
-		print(s, l+1, r);
-		swp(&(s[l]), &(s[i]));
+	    if(flag==0) {
+			swp(&(s[l]), &(s[i]));
+			print(s, l+1, r);
+			swp(&(s[l]), &(s[i]));
 	    }
-
 	}
 }
-}
 
 
-int main()
-{
+int main() {
 	int t;
 	string s;
 	cin>>t;
